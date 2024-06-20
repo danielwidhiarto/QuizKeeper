7aa3fb2f59f42de6c6981ae6a0e0a774b21d9cda
danielwidhiarto <danielwidhiarto@gmail.com>
2024-06-20 11:42:25 +0700

    fix primary key on user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('username')->unique();
+            $table->string('username')->primary();
             $table->string('password');
             $table->string('role');
             $table->rememberToken();
