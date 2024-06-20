52cdff48d02876b0ad5f91fff5a3f68ae83835f8
danielwidhiarto <danielwidhiarto@gmail.com>
2024-06-20 11:43:11 +0700

    migrate and seed subjects table

--- database/migrations/2024_06_19_163807_subjects.php
+++ database/migrations/2024_06_19_163807_subjects.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('subject', function (Blueprint $table) {
-            $table->id('subject_code');
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->string('subject_code')->primary();
             $table->string('subject_name');
-        });
+        });        
     }
 
     /**
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('subject');
+        Schema::dropIfExists('subjects');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ComputerSeeder::class,
-            FileSeeder::class
+            FileSeeder::class,
+            SubjectSeeder::class
         ]);
     }
 }
