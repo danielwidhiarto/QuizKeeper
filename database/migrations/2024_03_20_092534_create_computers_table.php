2597d9263c9374cfe508bdf5d1f46530d1795309
danielwidhiarto <danielwidhiarto@gmail.com>
2024-07-11 17:17:17 +0700

    add room collumns

--- database/migrations/2024_03_20_092534_create_computers_table.php
+++ database/migrations/2024_03_20_092534_create_computers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('computers', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('room');
             $table->ipAddress('ip_address')->unique();
         });
     }
