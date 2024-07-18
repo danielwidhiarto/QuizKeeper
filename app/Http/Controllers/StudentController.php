251223caa7220e0e9ce521a42b12ada0a64fc273
danielwidhiarto <danielwidhiarto@gmail.com>
2024-07-18 14:36:00 +0700

    add uplaod file validation

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Computer;
 use App\Models\Files;
-use Illuminate\Validation\Rules\File;
 use Illuminate\Database\QueryException;
 
 class StudentController extends Controller
@@ -24,48 +23,55 @@ class StudentController extends Controller
         }
 
         $request->validate([
-            'file' => [
-                'required',
-                File::types(['zip'])
-                    ->max(12 * 1024 * 1024), // 12 MB
-            ],
+            'file' => 'required|mimes:zip|max:25600',
         ]);
 
-        $file = $request->file('file');
-        $fileName = $file->getClientOriginalName();
+        if ($request->file('file')->isValid()) {
 
-        if (strlen($fileName) > 255) {
-            return redirect()->back()->with('error', 'The file name is too long. Please use a shorter file name.');
-        }
-
-        try {
-            $existingFile = Files::where('computer_id', $computer->id)->first();
-
-            if ($existingFile) {
-                $existingFile->name = $fileName;
-                $existingFile->size = $file->getSize();
-                $existingFile->ip_address = $request->ip();
-                $existingFile->content = file_get_contents($file->getRealPath());
-                $existingFile->save();
-            } else {
-                $uploadedFile = new Files();
-                $uploadedFile->name = $fileName;
-                $uploadedFile->size = $file->getSize();
-                $uploadedFile->ip_address = $request->ip();
-                $uploadedFile->content = file_get_contents($file->getRealPath());
-                $uploadedFile->computer_id = $computer->id;
-                $uploadedFile->save();
+            $size = $request->file('file')->getSize();
+            
+            if ($size < 10240) {
+                return back()->withErrors(['file' => 'The file must be at least 10 KB.']);
             }
 
-            $fileDetails = [
-                'name' => $fileName,
-                'size' => $file->getSize(),
-                'uploaded_at' => now()->format('d F Y, H:i')
-            ];
+            $file = $request->file('file');
+            $fileName = $file->getClientOriginalName();
 
-            return redirect()->back()->with('success', 'File uploaded successfully.')->with('fileDetails', $fileDetails);
-        } catch (QueryException $e) {
-            return redirect()->back()->with('error', 'There was a problem uploading your file. Please try again.');
+            if (strlen($fileName) > 255) {
+                return redirect()->back()->with('error', 'The file name is too long. Please use a shorter file name.');
+            }
+
+            try {
+                $existingFile = Files::where('computer_id', $computer->id)->first();
+
+                if ($existingFile) {
+                    $existingFile->name = $fileName;
+                    $existingFile->size = $file->getSize();
+                    $existingFile->ip_address = $request->ip();
+                    $existingFile->content = file_get_contents($file->getRealPath());
+                    $existingFile->save();
+                } else {
+                    $uploadedFile = new Files();
+                    $uploadedFile->name = $fileName;
+                    $uploadedFile->size = $file->getSize();
+                    $uploadedFile->ip_address = $request->ip();
+                    $uploadedFile->content = file_get_contents($file->getRealPath());
+                    $uploadedFile->computer_id = $computer->id;
+                    $uploadedFile->save();
+                }
+
+                $fileDetails = [
+                    'name' => $fileName,
+                    'size' => $file->getSize(),
+                    'uploaded_at' => now()->format('d F Y, H:i')
+                ];
+
+                return redirect()->back()->with('success', 'File uploaded successfully.')->with('fileDetails', $fileDetails);
+            } catch (QueryException $e) {
+                return redirect()->back()->with('error', 'There was a problem uploading your file. Please try again.');
+            }
+        } else {
+            return redirect()->back()->with('error', 'Invalid file upload. Please try again.');
         }
     }
 }
